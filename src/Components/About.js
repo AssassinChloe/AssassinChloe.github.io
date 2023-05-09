2c471f94f860009b618c326aae250ecdf0a4bea6
Chloe ASSASSIN <cassassi@e2r2p2.clusters.42paris.fr>
2023-05-09 17:34:49 +0200

    jamais 2 sans 3

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,7 +22,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="Nordic Giant Profile Pic"
+                alt="Myself"
               />
             </div>
             <div className="nine columns main-col">
