aa799f9ac1028253fb84c6cfcc50fb3da4874867
Chloé ASSASSIN <clotus@hotmail.fr>
2023-05-13 21:45:21 +0200

    ajout emerald sense et certif codingame

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,6 +15,7 @@ class Portfolio extends Component {
           <div className="item-wrap">
             <Zmage alt={projects.title} src={projectImage} />
             <div style={{ textAlign: "center" }}>{projects.title}</div>
+            <div>{projects.description}</div>
           </div>
         </div>
       );
