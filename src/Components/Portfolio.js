9fbd9a38137c11b56294d574154642a7a3378c53
Chloe ASSASSIN <cassassi@e2r8p2.clusters.42paris.fr>
2023-05-15 12:24:04 +0200

    .

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -26,7 +26,7 @@ class Portfolio extends Component {
         <Fade left duration={1000} distance="40px">
           <div className="row">
             <div className="twelve columns collapsed">
-              <h1>Quelques uns des mes travaux en cours</h1>
+              <h1>Quelques uns de mes travaux en cours</h1>
 
               <div
                 id="portfolio-wrapper"
